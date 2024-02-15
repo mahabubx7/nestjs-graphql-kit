361c7144a0a52d2347f001a28be989777043afb6
Mahabub <mahabubx7@gmail.com>
2024-02-15 07:24:15 +0600

    :truck: chore(db): implemented typeorm initials

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { loadEnvVariables } from '@config/variables';
+import { dbProviders } from '@config/database';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [loadEnvVariables],
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ...dbProviders],
 })
 export class AppModule {}
